888d0bf13dee1c2ee45823d99539f128f2f2b283
Rishabsingh7 <rishabsingh78089@gmail.com>
2025-12-21 23:44:02 +0530

    Added fourth class examples

---
+++ JS_Programming/fourClass/function.js
@@ -0,0 +1,24 @@
+/** 
+ *  Function:- set of instruction/statements to perform the particular task.
+ * 
+ *   Define:-
+ *          1)Function Declaration
+ *     
+ *     Syntax: Function functionName (ParameterList)
+ *           {
+ *             //body
+ *            }
+ * 
+ *      eg:         let a=10;
+ *                  let b=20;
+ *                 function add(n1, n2)
+ *                    {
+ *                        let result=n1+n2;
+ *                        console.log(result);
+ *                    }
+ * 
+ * 
+ *          // calling the function
+ *           add(a,b)
+ */
+console.log("Hello");
