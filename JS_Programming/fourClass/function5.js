c5247f611215989998f5176b47c94dc472b10010
Rishabsingh7 <rishabsingh78089@gmail.com>
2025-12-22 12:38:28 +0530

    modifiy some function files and add some files

---
+++ JS_Programming/fourClass/function5.js
@@ -0,0 +1,9 @@
+let a=10;
+let b=20;
+
+let  addition=(a,b)=>
+           {
+            let result=a+b;
+            console.log(result);
+           } 
+addition(a,b)
---
+++ JS_Programming/fourClass/function6.js
@@ -0,0 +1,10 @@
+let a=10;
+let b=20;
+
+
+add(a,b)
+function add(a,b)
+{ 
+    let result=a+b;
+    console.log(result);
+}
