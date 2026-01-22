4d9903b59a3f19e6dbc4dbb67ca9c323966c26cf
Rishabsingh7 <rishabsingh78089@gmail.com>
2026-01-22 10:51:36 +0530

    complete the react element concept

---
+++ JS_Programming/react-element/script.js
@@ -0,0 +1,35 @@
+// const container = React.createElement(
+//   'section',
+//   { className: 'sections', id: 'section1' },
+//   [
+//     React.createElement(
+//       'h2',
+//       { className: 'subheadings', id: 'subheading1', key: 'title' },
+//       'Valtriix IT Solutions'
+//     ),
+//     React.createElement(
+//       'p',
+//       { className: 'paragraphs', id: 'para1', key: 'desc' },
+//       'Welcome to Valtriix IT Solutions, We Provide Software solutions'
+//     ),
+//     React.createElement(
+//       'img',
+//       {
+//         className: 'images',
+//         id: 'img1',
+//         src: 'https://cdn.worldvectorlogo.com/logos/react-1.svg',
+//         height:200,
+//         alt: 'Company Logo',
+//         key: 'image'
+//       }
+//     )
+//   ]
+// );
+
+// const root = ReactDOM.createRoot(
+//   document.querySelector('#root')
+// );
+
+// root.render(container);
+
+console.log("Hello World!!");
